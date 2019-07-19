5c7b9fdfa609ae177005ef24b7017358f5468747
Ryan Hazzard <rhazzardphoto@gmail.com>
2019-07-19 10:21:29 -0700

    plays intro and fades out, then appends text but doesnt fade in

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,24 @@ end.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
         $('#delayed').append(`<h1 id="delay-font" class="pb-0 mb-0" >and this is my Porfolio</h1>`)
     });
 
-var finished = $('#delayed  ')
+var finished = $('#delayed')
 
-finished.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
-    function (e) {
-        console.log('on end worked'); 
-        $('.container-fluid').fadeOut(500, function() {
-            $(this).empty().show();
-         });
-         
-    });
+finished.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',function (e) {
+            $('#top-container').fadeOut(500, function () {
+                $(this).empty().show();
+                
+                    $(this).fadeIn('slow', function(){
+                        $(this).html(`<h2>Bot Works`)
+                    })
+            });
+        
+            $('#bot-container').fadeOut(500, function () {
+                $(this).empty().show();
+                
+                    $(this).fadeIn('slow', function(){
+                        $(this).html(`<h2>Bot Works`)
+                    })
+            });
+            });
+        
+
