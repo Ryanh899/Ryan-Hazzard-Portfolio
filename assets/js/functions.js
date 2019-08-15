e5d51c0a4a67625bbd10a344153b0d72768c7418
Ryan Hazzard <rhazzardphoto@gmail.com>
2019-08-15 14:22:11 -0700

    fadeOut isnt smoothest but works, summary appended

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,19 +1,45 @@
+//appends first fade in
 function append(elem) {
     $(elem).append(`<h1 class="animated fadeIn firstFade">Hey, I'm Ryan, Web Developer</h1>`)
 };
-
+//appends second fade in
 function appendTwo(elem) {
     $(elem).append(`<h1 class="animated fadein slow secondFade delay-3.5s">and this is my portfolio`)
 };
-
-function appendSummary(...elems) {
-    $(elems[0]).fadeOut(500, () => {
-        $(this).empty().show(); 
-    }); 
-    $(elems[1]).fadeOut(500, () => {
-        $(this).empty().show(); 
-    }); 
+//fadeout/empty
+function endFade(...elems) {
+    $(elems[0]).fadeOut(200)
+    $(elems[1]).fadeOut(200)
 };
 
 
 
+async function displayPage(func, elem2) {
+    $.when(func).then(appendSummary(elem2)); 
+};
+
+
+
+function appendSummary(elem) {
+        $(elem).html(`<div class="row">
+        <div class="col-sm-5 pt-2 mt-2">
+            <a href="#" class="photo">
+                <img class="p-3 m-3 image-fluid pt-4  animated fadeIn delay-3.5s" id="bio-pic"
+                    src="./assets/images/HAZ_1501.jpg" />
+                <div class="glow-wrap">
+                    <i class="glow"></i>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-7">
+            <p id="page-title" class="text-center mr-5 pr-4 pt-5 mt-5 animated fadeIn delay-3.5s">Summary</p>
+            <P id="page-content" class="text-center mr-5 pr-4"> Full-stack developer proficient with the MERN stack.
+                Along with being a web-developer, works as a professional photographer and is proficient with Adobe
+                Lightroom and Adobe Photoshop CC. Is an Eagle Scout. Experienced in HTML5, CSS3, Javascript, Jquery,
+                Bootstrap, Firebase, AJAX, and Axios. Three years-experience working in customer service and
+                proficient in Spanish.
+            </P>
+        </div>
+    </div>
+    </div>`)
+    }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,27 @@
+//hide page
+$('#hiddenSummary').hide();
+
+//append first 
 append('#container');
 
+//append second on first-end
 $('#container').on('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
     appendTwo('#delayed');
 });
 
+//fadeout/empty both
 $('#delayed').on('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-    appendSummary('#top-container', '#bot-container'); 
-}); 
+    displayPage(endFade('#onFadeOut', '#delayed'), '#top-container')
+    // endFade('#top-container', '#bot-container')
+});
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
+// append summary 
