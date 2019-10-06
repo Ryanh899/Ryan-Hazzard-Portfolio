0726f85a5bdfd145185491f55cb4f30aa85c52a3
Ryan Hazzard <rhazzardphoto@gmail.com>
2019-10-05 22:41:56 -0700

    presentable

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -1,19 +1,8 @@
-// //hide page
-// $('#hiddenSummary').hide();
-
-//append first 
-append('#container');
-
-//append second on first-end
-$('#container').on('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-    appendTwo('#delayed');
+$('#introName').one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
+    $('#description').css('display', 'block')
 });
 
-//fadeout/empty both
-$('#delayed').on('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
-    displayPage(endFade('#onFadeOut', '#delayed', '#top-container'), '#hidden')
-    // endFade('#top-container', '#bot-container')
-});
+
 
 // // append summary 
 
